308f18bcfba3b4d88c42ccc773693b8c84fcebd4
karthikreddydasari <141440717+karthikreddydasari@users.noreply.github.com>
2024-09-25 22:19:37 +0530

    Add files via upload

---
+++ Prime Factorization.py
@@ -0,0 +1,15 @@
+import math
+def prime_factors(num):
+    while num % 2 == 0:
+        print(2)
+        num = num / 2
+
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        while num % i == 0:
+            print(i)
+            num = num / i
+    if num > 2:
+        print(int(num))
+
+num = 7
+prime_factors(num)
